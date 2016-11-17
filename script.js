c4f26a76e64319993648c408b9f869462153b8e6
Lindsey Kopacz <lkopacz@Lindseys-MacBook-Pro.local>
2016-11-17 18:20:39 -0500

    getting computer random generated position

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-var checkClicked = false;
+var checkClicked = true;
 $( "div" ).click(function() {
-  if (checkClicked === false && $(this).hasClass("clicked")) {
+  if (checkClicked === true && $(this).hasClass("clicked")) {
     alert("Please select a blank square");
   }
   $(this).addClass("clicked");
-  var position = $(".clicked span").position();
-  console.log(position.left);
+  var position = $(this).position();
+  console.log((position.left)-8 + 'px');
+  console.log((position.top)-8 + 'px');
 });
+
+function computerTurn(){
+  var randomLeft = Math.floor(Math.random() * 600);
+  var randomTop = Math.floor(Math.random() * 600);
+  console.log(randomLeft);
+  console.log(randomTop);
+}
+
+computerTurn();
