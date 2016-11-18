d94bbdb3340614811c80faa4dbd59336cc2e12d2
Lindsey Kopacz <lkopacz@Lindseys-MacBook-Pro.local>
2016-11-17 19:48:26 -0500

    still working on it

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 var checkClicked = true;
-$( "div" ).click(function() {
-  if (checkClicked === true && $(this).hasClass("clicked")) {
-    alert("Please select a blank square");
-  }
-  $(this).addClass("clicked");
-  var position = $(this).position();
-  console.log((position.left)-8 + 'px');
-  console.log((position.top)-8 + 'px');
-});
+function getPlayerPosition() {
+  $( "div" ).click(function() {
+    if (checkClicked === true && $(this).hasClass("clicked")) {
+      alert("Please select a blank square");
+    }
+    $(this).addClass("clicked");
+    var position = $(this).position();
+    var playerLeftMin = position.left-8;
+    var playerTopMin = position.top-8;
+    var playerRightMax = playerLeftMin + 200;
+    var playerBottomMax = playerTopMin + 200;
+    var playerDimensions = [playerLeftMin, playerTopMin];
+  });
+}
+
+var playerPosition = getPlayerPosition();
+
+
 
 function computerTurn(){
   var randomLeft = Math.floor(Math.random() * 600);
   var randomTop = Math.floor(Math.random() * 600);
-  console.log(randomLeft);
-  console.log(randomTop);
+  var randomPosition = [randomLeft, randomTop];
 }
 
-computerTurn();
+var computerPosition = computerTurn();
+
+function testPostion() {
+  if
+}
